fae502cf620caeacf11a132324052335f6c78c02
Meagan <olsenme@oregonstate.edu>
2019-10-08 01:39:10 -0700

    Initial Commit

---
+++ myTipCalc/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  myTipCalc
+//
+//  Created by Meagan Olsen on 10/8/19.
+//  Copyright © 2019 Meagan Olsen. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
