f21cddeebbcf89f3119f9027acbff433d114e634
Meagan <olsenme@oregonstate.edu>
2019-10-08 03:05:25 -0700

    adding completed app

--- myTipCalc/ViewController.swift
+++ myTipCalc/ViewController.swift
@@ -9,12 +9,33 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
-
+    
+    @IBOutlet weak var billField: UITextField!
+    @IBOutlet weak var tipControl: UISegmentedControl!
+    @IBOutlet weak var tipLabel: UILabel!
+    @IBOutlet weak var totalLabel: UILabel!
+    
+    @IBAction func onTap(_ sender: Any) {
+        view.endEditing(true)
+    }
+    @IBAction func calculateTip(_ sender: Any) {
+        let tipPercentages = [0.1,0.18,0.2]
+        //get the bill
+        let bill = Double(billField.text!) ?? 0
+        //calc tip and total
+        let tip = bill * tipPercentages[tipControl.selectedSegmentIndex]
+        let total = bill + tip
+        //update tip and total label
+        tipLabel.text = String(format: "$%.2f",tip)
+        totalLabel.text=String(format: "$%.2f",total)
+        
+        
+    }
 }
 
